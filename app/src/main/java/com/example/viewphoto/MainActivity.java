616e074cb591e15c878fc2c588e24491ed7fe28c
Ildar Minigulov <mig.ildar@gmail.com>
2020-04-25 20:16:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/viewphoto/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.viewphoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go_to(View view) {
+
+    }
+
+    public void back(View view) {
+
+    }
+}
