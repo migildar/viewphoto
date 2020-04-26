05a05fe32ee48e78a19d61a0dad86857eb681abf
Ildar Minigulov <mig.ildar@gmail.com>
2020-04-26 11:49:48 +0300

    add fix

--- app/src/main/java/com/example/viewphoto/MainActivity.java
+++ app/src/main/java/com/example/viewphoto/MainActivity.java
@@ -2,22 +2,41 @@ package com.example.viewphoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        textView = findViewById(R.id.tv_message);
+        int randomNumberUsingNextInt = getRandomNumberUsingNextInt(0, 100);
+        textView.setText("http://myfile.org/" + randomNumberUsingNextInt);
     }
 
     public void go_to(View view) {
-
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 
     public void back(View view) {
+        finish();
+    }
 
+    public int getRandomNumberUsingNextInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
     }
 }
